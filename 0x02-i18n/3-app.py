8c4561d239c476cf75c86558be668b28aa906f7c
Michelle-chats <you@example.com>
2023-08-01 21:31:38 +0200

    submitting

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
-"""Parametrize templates
 """
-from flask import Flask
+Parametrize templates
+"""
+
+import babel
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from flask import render_template, request
-
-
-class Config:
-    """Flask Babel configuration.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """ _ or gettext function to parametrize your templates
+class Config:
     """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+     determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
